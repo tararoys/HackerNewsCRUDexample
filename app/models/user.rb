1f0c6ef162374cb3213f38cd6a53630c82268e91
Tara Roys <tlroys@gmail.com>
2014-02-26 15:28:24 -0600

    Usernames are now unique
    Created a unique validation in 'rubyland.'
    
    An activerecord validation is like a doorman, and the database is like a building.  The doorman makes sure to throw out anybody that doesn't belong before they get past the door.
    
    An activerecord table constraint is like everybody in the building looking at
    a stranger and knowing they don't belong, so they get together and throw
    the stranger out.
    
    The first is less secure than the latter.  The latter is really hard to change later. Changing a database constraint is like trying to change a community's dislike of strangers, while changing an activerecord validation is as easy as hiring a new doorman.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
   
+ validates :name, :uniqueness => true
+  
  include BCrypt
  
  def password 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -17,7 +17,8 @@ describe User do
 
   it "should have a unique name" do 
     User.create(name: "admin")
-    expect{User.create(name:"admin", password_hash:"password")}.to raise_error(ActiveRecord::RecordNotUnique)
+    admin = User.new(name:"admin", password_hash:"password")
+    expect{admin.save(validate: false)}.to raise_error(ActiveRecord::RecordNotUnique) #skipping ruby validations in order to raise database constraint error
   end
 
   it "should have a hashed password" do 
@@ -29,6 +30,19 @@ describe User do
     expect(tara.password).to eq("password") #password works due to bcrypt comparison magic. Yay bcrypt comparison magic!
 
   end
+
+    it "should not have duplicate name" do
+      # question: when do the validations occur?
+      # According to alyssa, validations when save is called. So you can create an instance of the user object, but if that object is invalid, you can't save it to the database. 
+      # does it stop models being created? (user 2 doesn't exist?)
+      # Does it run on .save / .create?
+      # Does it run on .new?
+
+      u1 = User.create(name: "Debra", password: "1234")
+      u2 = User.create(name: "Debra", password: "1234")
+      expect(User.where(name: "Debra").count).to eq(1)
+    end
+
 
 
 end
