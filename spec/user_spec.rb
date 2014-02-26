5955a27b9c172658cbb37e3782af7dd1eb226427
Tara Roys <tlroys@gmail.com>
2014-02-26 09:31:39 -0600

    User ought to have a password_hash

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe User do
   it "should have a name" do
-    tara = User.new(name:"Tara");
+    tara = User.new(name:"Tara")
     expect(tara.name).to eq("Tara") 
   end
+
+  it "should have a password_hash" do
+    tara = User.new(name: "Tara", password_hash: "password")
+    expect(tara.password_hash).to eq("password")
+  end 
 end
