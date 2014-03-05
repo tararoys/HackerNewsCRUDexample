86575c7258ddc26783a1fe8893fe0517f08978ad
Tara Roys <tlroys@gmail.com>
2014-03-04 19:50:23 -0600

    How do I allow a user to login?
    This code was written based on the following login user story;
    
    1. On the home page, click on login.  This takes you to the login page.
    2. On the login page, enter e-mail and passsword, and click login. This takes you to the home page with the title bar changed to show your name and a logout link
    
    The login flow is like ali baba's cave demanding a password before it will let you enter and view the marvelous things.

---
+++ app/controllers/users.rb
@@ -0,0 +1,15 @@
+get '/users/login' do 
+  erb :login
+end
+
+post '/users/login' do 
+
+  user = User.find_by_email(params["user"]["email"])
+  if user && user.password == params["user"]["password"]
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+
+end
