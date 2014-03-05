06e816e355afeedbccf64d155445bdc115bce7b7
Tara Roys <tlroys@gmail.com>
2014-03-04 19:52:42 -0600

    How do I make it easy to test the login flow?
    Add a seeds file.
    
    A seeds file is like a person putting fake things on their resume to make the resume look more attractive than it actually is.

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+        zoe = User.create(name: "zoe", email: "zoe@serenity.com", password: "password")
+        inara = User.create(name: "inara", email: "inara@serenity.com", password: "password")
+
+        hate_the_alliance = Post.create(title: "I hate the alliance", source: "We will rise again.", author: zoe)
+        i_said_dont = Post.create(title: "Don't come in my shuttle", source: "The exact words I used were don't", author: inara)
+
+
+        zoe_hates_alliance = Comment.create(user_id: zoe.id, post_id: hate_the_alliance.id, content: "You have a problem with your brain being missing")
+        zoe_knows_ruby = Comment.create(user_id: zoe.id, post_id: i_said_dont.id, content: "You tell the captain, ambassador.")
+
+        inara_likes_alliance_money = Comment.create(user_id: inara.id, post_id: hate_the_alliance.id, content: "Sadly, they have the money. ") 
+        inara_hates_mal = Comment.create(user_id: inara.id, post_id: i_said_dont.id, content: "Why is it so hard to understand this basic rule?") #for some reason, inara's id is being defined as nil in the database
+      
