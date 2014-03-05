6ba7ddefe230a78f2af095931c777e4b9de76b45
Tara Roys <tlroys@gmail.com>
2014-03-05 13:40:09 -0600

    How do I edit a comment?
    Flow:
    On any page with a comment,
      if the comment was written by a currently logged-in user
      add an edit button
    When you click the edit button,
      you are taken to the eit comment page.
    
    Edit the comment and click the button.
    You are taken back to the page you originally came from.
    
    NOTE: I only implemented this for the Post page, and it takes you
    back to the post page. I deliberately ingnored the comments page because
    I intend to extract the comment functionality out into what's called a
    'partial.'  I haven't implemented a partial before. I'm excited!

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -8,3 +8,14 @@ post "/comment/new" do
   comment = Comment.create(params[:comment])
   redirect "/post/#{params[:comment][:post_id]}"
 end
+
+get "/comment/:id/edit" do
+  @comment = Comment.find(params[:id])
+  puts @comment.inspect
+  erb :edit_comment
+end
+
+post "/comment/:id/edit" do
+  comment = Comment.update(params[:id], content: params[:comment][:content])
+  redirect "/post/#{comment.post.id}"
+end
--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,4 +1,5 @@
 get "/post/:id" do
+  @user = User.find(session[:user_id]) if session[:user_id] 
   @post = Post.find(params[:id])
   @comments = @post.comments.order(:created_at).reverse_order
   erb :post
